34dce888d9a250cee34e41e38fb60624c2fa4394
andy23512 <andy23512@gmail.com>
2019-11-02 22:42:33 +0800

    Working example with jest + puppeteer

---
+++ test/app.test.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+describe('app', () => {
+  beforeEach(async () => {
+    await page.goto('https://google.com')
+  })
+
+  it('should display a google text', async () => {
+    await expect(page).toMatch('google')
+  })
+})
